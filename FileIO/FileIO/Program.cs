8bccb4d6d0e3771d4c9f9e2371493ef2d7c3e21f
Unknown <billhuang0916@gmail.com>
2018-06-13 14:57:59 -0500

    Added File Reading

--- FileIO/FileIO/Program.cs
+++ FileIO/FileIO/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 
 namespace FileIO
 {
@@ -8,7 +10,32 @@ namespace FileIO
         {
             Console.WriteLine("Enter Name of Data File: ");
             String fileName = Console.ReadLine();
+            ArrayList nums = new ArrayList();
+            Int32 sum = 0;
 
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Int32 curNum = Int32.Parse(line);
+                        nums.Add(curNum);
+                        sum += curNum;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
