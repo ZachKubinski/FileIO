61511c45b291bd60acd10473d774d97b78b5a4c2
Unknown <zachkbc@gmail.com>
2018-06-13 14:43:01 -0500

    init

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name of Data File: ");
+
+        }
+    }
+}
